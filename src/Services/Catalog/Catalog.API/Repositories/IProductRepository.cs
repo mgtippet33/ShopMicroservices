7d9d2066512d4fd8717882bff8196135e6f7214a
mgtippet33 <mgtippet33@gmail.com>
2023-08-11 20:27:26 +0300

    add Catalog service

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+
+        Task<Product> GetProductAsync(string id);
+
+        Task<IEnumerable<Product>> GetProductByNameAsync(string name);
+
+        Task<IEnumerable<Product>> GetProductByCategoryNameAsync(string categoryName);
+
+        Task CreateProduct(Product product);
+
+        Task<bool> UpdateProduct(Product product);
+
+        Task<bool> DeleteProduct(string id);
+    }
+}
